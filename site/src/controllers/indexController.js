bb958f4e6b8235f5a8e231a558da432d1eb0fbd7
tonylumia710 <tonylumia710@hotmail.com>
2021-11-14 13:30:13 -0300

    funcionalidad boton ver todos los prod. y carga de vista productos

--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -3,6 +3,7 @@ let db = require('../database/models');
 const { Op } = require('sequelize');
 const { products } = require("./adminController");
 
+
 module.exports = {
     index: (req, res) => {
         db.Product.findAll({ //Encontra todos los productos
@@ -15,6 +16,26 @@ module.exports = {
         })
         .then(products => {
           res.render("index", {
+              sliderTitle: "Ofertas especiales",
+              sliderProducts: products,
+              carousel,
+              session: req.session,
+            })
+        })
+        .catch(error => console.log(error))
+    },
+
+    productos: (req, res) => {
+        db.Product.findAll({ //Encontra todos los productos
+            where: { // donde
+                discount: { // el valor de la columna (database) "discount"
+                    [Op.gte]: 0 // sea mayor o igual a 5
+                }
+            },
+            include: [{association: "images"}]
+        })
+        .then(products => {
+          res.render("productos.ejs", {
               sliderTitle: "Ofertas especiales",
               sliderProducts: products,
               carousel,
--- site/src/routes/index.js
+++ site/src/routes/index.js
@@ -7,5 +7,6 @@ router.get('/', cookieCheck, controller.index);
 router.get('/comprar', cookieCheck, controller.comprar);
 router.get('/envios', cookieCheck, controller.envios);
 router.get('/contacto', cookieCheck, controller.contacto);
+router.get('/productos', cookieCheck, controller.productos);
 
 module.exports = router;
