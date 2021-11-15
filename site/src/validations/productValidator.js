3e24aa282ce1365d7a42abafe67c1e938912363b
unknown <javiriki31@hotmail.com>
2021-11-15 18:26:48 -0300

    arreglo validacion editproduct back kend

--- site/src/validations/productValidator.js
+++ site/src/validations/productValidator.js
@@ -6,7 +6,6 @@ module.exports = [
     check('price').notEmpty().withMessage('Este campo no puede estar vacío').isNumeric().withMessage('Solo puedes ingresar números'),
     check('discount').notEmpty().withMessage('Este campo no puede estar vacío'),
     check('description').notEmpty().withMessage('Este campo no puede estar vacío').isLength({min: 20}).withMessage('Debe tener como mínimo 20 caracteres'),
-    check('category').notEmpty().withMessage('Este campo no puede estar vacío'),
     check('measures').notEmpty().withMessage('Este campo no puede estar vacío'),
     check('origin').notEmpty().withMessage('Este campo no puede estar vacío'),
     
