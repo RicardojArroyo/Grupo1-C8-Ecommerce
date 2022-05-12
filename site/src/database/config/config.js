faa73944b211c411074715ab16e8395b848c359c
tonylumia710 <tonylumia710@hotmail.com>
2022-05-12 02:25:14 -0300

    base de datos subida a  clever-cloud y funcionando

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "muebleria_imperio_db",
-    "host": "127.0.0.1",
+    "username": "uby2jnc79msffpor",
+    "password": "G11IKb52QQJsyDFU0wFq",
+    "database": "bwp5azxe9wceufi6zqlx",
+    "host": "bwp5azxe9wceufi6zqlx-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "uby2jnc79msffpor",
+    "password": "G11IKb52QQJsyDFU0wFq",
+    "database": "bwp5azxe9wceufi6zqlx",
+    "host": "bwp5azxe9wceufi6zqlx-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "uby2jnc79msffpor",
+    "password": "G11IKb52QQJsyDFU0wFq",
+    "database": "bwp5azxe9wceufi6zqlx",
+    "host": "bwp5azxe9wceufi6zqlx-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   }
 }
